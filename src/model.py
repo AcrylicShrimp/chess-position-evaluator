dcfc6a9bf18e8aea03b34325d5a11a73dbf8a753
AcrylicShrimp <led789zxpp@naver.com>
2025-07-06 20:53:19 +0900

    fix: correct model

--- src/model.py
+++ src/model.py
@@ -22,51 +22,53 @@ class ResidualBlock(torch.nn.Module):
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.block = torch.nn.Sequential(
-            torch.nn.Conv2d(20, 256, kernel_size=3, padding=1),
+        self.initial_block = torch.nn.Sequential(
+            torch.nn.Conv2d(20, 256, kernel_size=3, padding=1, bias=False),
+            torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            torch.nn.ReLU(inplace=True),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            torch.nn.Conv2d(256, 32, kernel_size=1),
         )
 
+        self.residual_blocks = torch.nn.Sequential(
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+            ResidualBlock(256),
+        )
+
+        self.gap = torch.nn.AdaptiveAvgPool2d((1, 1))
+
+        head_input_size = 256
+
         self.cp_head = torch.nn.Sequential(
-            torch.nn.Linear(32 * 8 * 8, 1024),
+            torch.nn.Linear(head_input_size, 1024),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(1024, 512),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(512, 1),
         )
         self.mate_head = torch.nn.Sequential(
-            torch.nn.Linear(32 * 8 * 8, 1024),
+            torch.nn.Linear(head_input_size, 1024),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(1024, 512),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(512, 3),
-            torch.nn.Tanh(),
         )
 
-    def forward_cp(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.block(x)
-        out = out.reshape(-1, 32 * 8 * 8)
-        return self.cp_head(out)
-
-    def forward_mate(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.block(x)
-        out = out.reshape(-1, 32 * 8 * 8)
-        return self.mate_head(out)
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return torch.cat([self.forward_cp(x), self.forward_mate(x)], dim=1)
+        out = self.initial_block(x)
+        out = self.residual_blocks(out)
+        out = self.gap(out)
+        out = torch.flatten(out, 1)
+
+        cp_out = self.cp_head(out)
+        mate_out = self.mate_head(out)
+
+        return torch.cat([cp_out, mate_out], dim=1)
