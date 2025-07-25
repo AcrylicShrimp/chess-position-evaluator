4d1b2b415d37f2e465e9a3c34a2e315ae8b6167d
AcrylicShrimp <led789zxpp@naver.com>
2025-07-25 19:36:12 +0900

    feat: make model small

--- train-eval/model.py
+++ train-eval/model.py
@@ -23,36 +23,46 @@ class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.initial_block = torch.nn.Sequential(
-            torch.nn.Conv2d(20, 256, kernel_size=3, padding=1, bias=False),
-            torch.nn.BatchNorm2d(256),
+            torch.nn.Conv2d(20, 64, kernel_size=3, padding=1, bias=False),
+            torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(inplace=True),
         )
 
         self.residual_blocks = torch.nn.Sequential(
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
-            ResidualBlock(256),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
+            ResidualBlock(64),
         )
 
         self.gap = torch.nn.AdaptiveAvgPool2d((1, 1))
 
-        head_input_size = 256
+        head_input_size = 64
 
         self.cp_head = torch.nn.Sequential(
-            torch.nn.Linear(head_input_size, 1024),
+            torch.nn.Linear(head_input_size, 512),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(1024, 512),
+            torch.nn.Linear(512, 256),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(512, 1),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(128, 1),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
